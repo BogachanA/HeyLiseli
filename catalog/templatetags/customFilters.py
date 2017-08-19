5da4e69ed37f520f727c6231d3901fcd10506f05
boğaçhan arslan <bogacbugra@gmail.com>
2017-08-19 11:43:42 +0300

    Implemented customFilters.py for custom filters, and added a substring filter. Altered company details page to only display social links if that link exists.

---
+++ catalog/templatetags/customFilters.py
@@ -0,0 +1,12 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name='substring',is_safe=True)
+@stringfilter
+def substring(value,arguments):
+    args=arguments.split(",")
+    start=int(args[0])
+    end=int(args[1])
+    return value[start:end]
