b193c15835657b3437724b4174307ebb9fa7c359
boğaçhan arslan <bogacbugra@gmail.com>
2017-09-16 17:53:07 +0300

    Completed the project info page. Completed the student project models. Ready to deploy.

--- studentAds/urls.py
+++ studentAds/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^info/(\d+)/',views.project_details, name="proje_detay"),
+    url(r'^$',views.projects_main,name='proje_ana'),
 ]
--- studentAds/views.py
+++ studentAds/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import *
+from django.db import IntegrityError
+from django.db.models import Q
 
 # Create your views here.
 def project_details(request,project_id):
     project=get_object_or_404(StudentProject, id=project_id)
+    try:
+        if request.user.is_authenticated():
+            view_key=request.user.id
+        else:
+            if not request.session.get('has_session'):
+                request.session['has_session'] = True
+            view_key=request.session.session_key
 
+        if not ProjectView.objects.filter(rel_project=project, 
+                                      session_key_or_user_id=view_key):
+            ProjectView.objects.create(rel_project=project,
+                                   session_key_or_user_id=view_key,
+                                   date=timezone.now(),
+                                   ip=request.META['REMOTE_ADDR'])
+    except IntegrityError:
+        pass
+
+    current_view_count = ProjectView.objects.filter(rel_project=project).count()
     context={
         'project':project,
     }
 
     return render(request,'project_detail.html',context)
+
+def projects_main(request):
+    categories=ProjectCategory.objects.all()
+    context={
+        'categories':categories,
+    }
+    return render(request,'projects_main.html',context)
