04ad3367cc425767fbccc0cac68426e71af33ac3
boğaçhan arslan <bogacbugra@gmail.com>
2017-09-23 16:25:05 +0300

    installed whitenoise and added middleware for staticfiles in production

--- HeyLiseli/settings.py
+++ HeyLiseli/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware', #Debug toolbar
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
