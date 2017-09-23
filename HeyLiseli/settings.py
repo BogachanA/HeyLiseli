5e05d8f8ecf197139a2e9da2f082e001dcfbe349
boğaçhan arslan <bogacbugra@gmail.com>
2017-09-23 16:42:36 +0300

    reversed change in settings.py database host

--- HeyLiseli/settings.py
+++ HeyLiseli/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'mydatabase',
         'USER': 'mydatabaseuser',
         'PASSWORD': 'mypassword',
-        'HOST': '*',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
