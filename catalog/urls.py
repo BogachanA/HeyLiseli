f86749d0c93d3f06b16b6cda630697fbcf0a6449
boğaçhan arslan <bogacbugra@gmail.com>
2017-10-28 19:08:01 +0300

    to test online with postgres. Lowered the resolution of landing page books image.

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^stajlar/staj-detay/(\d+)/',views.internship_detail, name="detay"),
     url(r'^stajlar/:query=(\w)/',views.internships,name='stajlar_harf'),
     url(r'^stajlar/', views.internships_main, name="stajlar"),
-    url(r'^gonullu-projeler/(\d+)', views.volunteer_detail,name='vol_detay'),
+    url(r'^gonullu-projeler/(\d+)/', views.volunteer_detail,name='vol_detay'),
     url(r'^gonullu-projeler/:query=(\w)/',views.volunteer_projects,name='vol_harf'),
     url(r'^gonullu-projeler/',views.volunteers,name='vol_ana'),
     url(r'^company/(\d+)/',views.company_detail, name="şirket_detay"),
